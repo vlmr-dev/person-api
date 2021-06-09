bd7add113cca4db598ff8de2a48f188fd44a80fd
vlmr-dev <vlmr.info@gmail.com>
2021-06-09 09:16:02 -0300

    Commit inicial

---
+++ src/test/java/com/github/vlmr/dev/person/PersonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vlmr.dev.person;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
