cfa94bb32e8d0a898b01674b549bd7efd7f8471d
vlmr-dev <vlmr.info@gmail.com>
2021-06-26 05:42:35 -0300

    Developed unit tests for create person

---
+++ src/test/java/com/github/vlmr/dev/person/service/PersonServiceTest.java
@@ -0,0 +1,48 @@
+package com.github.vlmr.dev.person.service;
+
+import com.github.vlmr.dev.person.dto.request.PersonDTO;
+import com.github.vlmr.dev.person.dto.response.MessageResponseDTO;
+import com.github.vlmr.dev.person.entity.Person;
+import com.github.vlmr.dev.person.repository.PersonRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static com.github.vlmr.dev.person.utils.PersonUtils.createFakeDTO;
+import static com.github.vlmr.dev.person.utils.PersonUtils.createFakeEntity;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class PersonServiceTest {
+
+    @Mock
+    private PersonRepository personRepository;
+
+    @InjectMocks
+    private PersonService personService;
+
+    @Test
+    void testGivenPersonDTOThenReturnSavedMessage() {
+        PersonDTO personDTO = createFakeDTO();
+        Person expectedSavedPerson = createFakeEntity();
+
+        when(personRepository.save(any(Person.class))).thenReturn(expectedSavedPerson);
+
+        MessageResponseDTO expectedSuccessMessage = createExpectedMessageResponse(expectedSavedPerson.getId());
+        MessageResponseDTO succesMessage = personService.createPerson(personDTO);
+
+        assertEquals(expectedSuccessMessage, succesMessage);
+    }
+
+    private MessageResponseDTO createExpectedMessageResponse(Long id) {
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + id)
+                .build();
+    }
+}
+
---
+++ src/test/java/com/github/vlmr/dev/person/utils/PersonUtils.java
@@ -0,0 +1,37 @@
+package com.github.vlmr.dev.person.utils;
+
+import com.github.vlmr.dev.person.dto.request.PersonDTO;
+import com.github.vlmr.dev.person.entity.Person;
+
+import java.time.LocalDate;
+import java.util.Collections;
+
+public class PersonUtils {
+
+    private static final String FIRST_NAME = "João";
+    private static final String LAST_NAME = "Silva";
+    private static final String CPF_NUMBER = "369.333.878-79";
+    private static final long PERSON_ID = 1L;
+    public static final LocalDate BIRTH_DATE = LocalDate.of(2010, 10, 1);
+
+    public static PersonDTO createFakeDTO() {
+        return PersonDTO.builder()
+                .firstName(FIRST_NAME)
+                .lastName(LAST_NAME)
+                .cpf(CPF_NUMBER)
+                .birthDate("04-04-2010")
+                .phones(Collections.singletonList(PhoneUtils.createFakeDTO()))
+                .build();
+    }
+
+    public static Person createFakeEntity() {
+        return Person.builder()
+                .id(PERSON_ID)
+                .firstName(FIRST_NAME)
+                .lastName(LAST_NAME)
+                .cpf(CPF_NUMBER)
+                .birthDate(BIRTH_DATE)
+                .phones(Collections.singletonList(PhoneUtils.createFakeEntity()))
+                .build();
+    }
+}
